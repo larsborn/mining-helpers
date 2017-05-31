951db7c1447eefb90bede9706819f8e728c2d067
Marc Schulz-Narres <git@marcsn.de>
2017-05-31 18:13:58 +0100

    moved balance project to seperate folder; created sample key file; replaced data analysis by database update

--- balance/filldb.py
+++ balance/filldb.py
@@ -3,16 +3,27 @@ import requests
 import configparser
 import krakenex
 import datetime
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from dateutil.parser import parse as parse_date
 from itertools import islice,groupby
+
+Base = declarative_base()
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 bankurl = config['DEFAULT']['bankurl']
 exchangeurl = config['DEFAULT']['exchangeurl']
 poolurl = config['DEFAULT']['poolurl']
+db_conn = config['DEFAULT']['db_conn']
 
-class mined(object):
+class mined(Base):
+    __tablename__ = 'mines'
+    tid     = Column(String(70), primary_key=True)
+    wallet  = Column(String(50))
+    time    = Column(DateTime)
+    amount  = Column(Float)
     def __init__(self, row):
         self.tid    = row[0]
         self.wallet = row[1]
@@ -27,7 +38,13 @@ class mined(object):
     def __eq__(self, other):
         return self.tid == other.tid
 
-class trade(object):
+class trade(Base):
+    __tablename__ = 'trades'
+    tid         = Column(String(50), primary_key=True)
+    time        = Column(DateTime)
+    amount_eth  = Column(Float)
+    amount_euro = Column(Float)
+    fee = Column(Float)
     def __init__(self, row):
         self.tid            = row[0]
         self.amount_eth     = float(row[1])
@@ -43,7 +60,11 @@ class trade(object):
     def __eq__(self, other):
         return self.tid == other.tid
         
-class deposit(object):
+class deposit(Base):
+    __tablename__ = 'deposits'
+    tid     = Column(String(50), primary_key=True)
+    time    = Column(DateTime)
+    amount  = Column(Float)
     def __init__(self, row):
         self.tid            = row[0]
         self.amount         = float(row[1])
@@ -57,7 +78,11 @@ class deposit(object):
     def __eq__(self, other):
         return self.tid == other.tid
 
-class withdrawal(object):
+class withdrawal(Base):
+    __tablename__ = 'withdrawals'
+    tid     = Column(String(50), primary_key=True)
+    time    = Column(DateTime)
+    amount  = Column(Float)
     def __init__(self, row):
         self.tid            = row[0]
         self.amount         = float(row[1])
@@ -71,7 +96,11 @@ class withdrawal(object):
     def __eq__(self, other):
         return self.tid == other.tid
         
-class SEPA(object):
+class SEPA(Base):
+    __tablename__ = 'sepas'
+    tid     = Column(String(50), primary_key=True)
+    time    = Column(DateTime)
+    amount  = Column(Float)
     def __init__(self, row):
         self.time           = parse_date(row[1]).replace(hour=23, minute=59)
         self.tid            = row[4]
@@ -149,60 +178,23 @@ result = trades + mines + sepas + withdrawals + deposits
 result.sort(key=lambda x: x.time, reverse=False)
 
 
-tradePresent = False
-income = 0
-eurobalance = 0
-girobalance = 0
-missing = 0
-untraded = 0
-eurofees = 0
-ethfees = 0
-missingtrades = 0
-missingincome = 0
+
+db = create_engine(db_conn)
+db.echo = False 
+Base.metadata.create_all(db)
+Session = sessionmaker(bind=db)
+session = Session()
+
+def insert_if_new(t):
+    s = tradesDB.select(tradesDB.c.tid == t.tid)
+    rs = db.execute(s)
+    if len(rs.fetchall()) == 0:
+        print("New Record with id %s" % t.tid)
+        i = tradesDB.insert()
+        db.execute(statement=i,tid=t.tid, time=t.time, amount_eth=t.amount_eth, amount_euro=t.amount_euro, fee=t.fee)
+    else:
+        print("Record with id %s already exists" % t.tid)
+
 for res in result:
-    if type(res) == SEPA:
-        if tradePresent and res.withdrawal == False:
-            tradePresent = False
-            girobalance += res.amount
-            print("[%s]\tGot %f € via SEPA. Giro Balance is %f €. Fees were %f €" % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount, girobalance,eurobalance-res.amount))
-            eurofees +=eurobalance-res.amount
-            eurobalance = 0
-        elif res.withdrawal == True:
-            girobalance += res.amount
-            print("[%s]\t%f € were withdrawn from Giro. Giro Balance is %f €." % (res.time.strftime("%Y-%m-%d %H:%M:%S"), -res.amount, girobalance))
-        else:
-            girobalance += res.amount
-            print("[%s]\tGot %f € via SEPA. Giro Balance is %f €. Fee is unknown due to missing trade." % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount, girobalance))
-            print("\t\t\tWARNING: There was no matching Trade. Balance was %f ETH." % income)
-            untraded += income
-            missingtrades+=1
-            income = 0
-    elif type(res) == deposit:
-        print("[%s]\tdeposit of %f ETH on Kraken." % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount))
-    elif type(res) == withdrawal:
-        print("[%s]\tWithdrawal of %f € from Kraken." % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount))
-    elif type(res) == mined:
-        income += res.amount
-        print("[%s]\tMined %f ETH. Balance is %f ETH" % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount, income))
-    elif type(res) == trade:
-        if income >0 and abs((1-(res.amount_eth / income))*100) >1:
-            print("[%s]\tTraded %f ETH for %f €. ETH transaction fee is unknown, Marketplace Fee was %f €" % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount_eth, res.amount_euro, res.fee))
-            print("\t\t\tWARNING: Balance was %f ETH but only %f ETH were traded." %(income,res.amount_eth))
-            missing += income-res.amount_eth
-            missingtrades+=1
-        elif income == 0:
-            print("[%s]\tTraded %f ETH for %f €. ETH transaction fee is unknown, Marketplace Fee was %f €" % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount_eth, res.amount_euro, res.fee))
-            print("\t\t\tWARNING: ETH Balance was 0 but ETH was traded. Some income is probably missing.")
-            missingincome += 1
-        else:
-            print("[%s]\tTraded %f ETH for %f €. Transaction fee was %f ETH, Marketplace Fee was %f €" % (res.time.strftime("%Y-%m-%d %H:%M:%S"), res.amount_eth, res.amount_euro, -(res.amount_eth-income),res.fee)) 
-            ethfees += -(res.amount_eth-income)
-        tradePresent = True
-        eurofees += res.fee
-        eurobalance += res.amount_euro
-        income = 0
-    #print(res)
-print("--------------------------------------------------------------------------")
-print("Kraken: %f ETH, Kraken: %f €, Giro: %f €, Sum: %f €" % (income,eurobalance,girobalance,eurobalance+girobalance))
-print("Fees: %f ETH, %f €" % (ethfees,eurofees))
-print("%f ETH got lost, %f ETH were untraded but probably withdrawn.\nAt least %i Trades seem to be missing. Also %i trades were carried out without balance." % (missing,untraded,missingtrades,missingincome))
+    session.add(res)
+session.commit()
